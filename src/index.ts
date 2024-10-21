e724efacd31c80c67070a932102fb9283e0f9020
Alex Coronell <alexcoronell@gmail.com>
2024-10-21 17:23:42 -0500

    Database conextion completed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import express from 'express'
+import "reflect-metadata";
+import app from "./app";
+import { AppDataSource } from "./db";
+const port = 3000;
 
-const app = express();
-const port = 3000
+async function main() {
+  try {
+    await AppDataSource.initialize();
+    app.listen(port);
+    console.log('Database connected');
+    console.log("Server is listening on port ", port);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-app.listen(port)
-console.log("Server is listening on port ", port);
+main();
