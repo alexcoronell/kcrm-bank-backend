b5edb16af411e85062501ea50988c755c223404c
Alex Coronell <alexcoronell@gmail.com>
2024-10-30 15:20:13 -0500

    biome installed

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { AppDataSource } from "./db";
 const port = 3000;
 
 async function main() {
-  try {
-    await AppDataSource.initialize();
-    app.listen(port);
-    console.log('Database connected');
-    console.log("Server is listening on port ", port);
-  } catch (e) {
-    console.error(e);
-  }
+	try {
+		await AppDataSource.initialize();
+		app.listen(port);
+		console.log("Database connected");
+		console.log("Server is listening on port ", port);
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 main();
