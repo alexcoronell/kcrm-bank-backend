74f7b03eb7a777d8c4dd6b2a08ad86acb5175127
Alex Coronell <alexcoronell@gmail.com>
2024-10-31 15:44:22 -0500

    Class validator applied on User Entity

--- src/entities/User.entity.ts
+++ src/entities/User.entity.ts
@@ -1,76 +1,82 @@
 import {
-	BaseEntity,
-	Column,
-	CreateDateColumn,
-	Entity,
-	JoinColumn,
-	ManyToOne,
-	OneToMany,
-	PrimaryGeneratedColumn,
-	UpdateDateColumn,
+  BaseEntity,
+  Unique,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from "typeorm";
 
 import { Exclude } from "class-transformer";
 
 import { Sale } from "./Sale.entity";
 import { UserType } from "./UserType.entity";
+import { IsBoolean, IsEmail, IsInt, isInt, IsNotEmpty, IsOptional, IsString, Min, MinLength } from "class-validator";
 
 @Entity()
+@Unique(["email"])
 export class User extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Column({ type: "varchar", length: "50" })
-	name: string;
+  @Column({ type: "varchar", length: "50" })
+  @IsNotEmpty({message: "name is required"})
+  @IsString()
+  readonly name: string;
 
-	@Column({ type: "varchar", length: 50 })
-	email: string;
+  @Column({ type: "varchar", length: 50 })
+  @IsNotEmpty({message: "email is required"})
+  @IsEmail()
+  email: string;
 
-	@Exclude()
-	@Column({ type: "varchar", length: 255 })
-	password: string;
+  @Exclude()
+  @Column({ type: "varchar", length: 255 })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8, {message: "must contain at least 8 characters"})
+  password: string;
 
-	@CreateDateColumn({
-		name: "created_at",
-		type: "timestamp",
-		default: () => "CURRENT_TIMESTAMP",
-	})
-	createAt: Date;
+  @CreateDateColumn({
+    name: "created_at",
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  createAt: Date;
 
-	@UpdateDateColumn({
-		name: "updated_at",
-		type: "timestamp",
-		default: () => "CURRENT_TIMESTAMP",
-	})
-	updateAt: Date;
+  @UpdateDateColumn({
+    name: "updated_at",
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  updateAt: Date;
 
-	@Column({
-		name: "active",
-		type: "boolean",
-		default: true,
-	})
-	active: boolean;
+  @Column({
+    name: "active",
+    type: "boolean",
+    default: true,
+  })
+  @IsBoolean()
+  @IsOptional()
+  active: boolean;
 
-	@Column({ type: "boolean", default: false })
-	deleted: boolean;
+  @Column({ type: "boolean", default: false })
+  deleted: boolean;
 
-	/************** RELATIONS **************/
-	@ManyToOne(
-		() => UserType,
-		(userType) => userType.users,
-	)
-	@JoinColumn({ name: "user_type" })
-	userType: number;
+  /************** RELATIONS **************/
+  @ManyToOne(() => UserType, (userType) => userType.users)
+  @JoinColumn({ name: "user_type" })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
+  userType: number;
 
-	@OneToMany(
-		() => Sale,
-		(sale) => sale.createdBy,
-	)
-	createdSales: Sale[];
+  @OneToMany(() => Sale, (sale) => sale.createdBy)
+  createdSales: Sale[];
 
-	@OneToMany(
-		() => Sale,
-		(sale) => sale.updatedBy,
-	)
-	updatedSales: Sale[];
+  @OneToMany(() => Sale, (sale) => sale.updatedBy)
+  updatedSales: Sale[];
 }
