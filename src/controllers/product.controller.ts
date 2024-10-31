b34ea99bac864fcf792c982497f168828c5e0c8f
Alex Coronell <alexcoronell@gmail.com>
2024-10-30 19:02:13 -0500

    Product service created and completed

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,98 @@
+import type { Request, Response } from "express";
+
+/* Entities */
+import { Product } from "../entities/Product.entity";
+
+/* Helpers */
+import pagination from "../helpers/pagination.helper";
+
+export const create = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const product = new Product();
+    product.name = name;
+    await product.save();
+    return res.status(201).json(product);
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({ message: e.message });
+    }
+  }
+};
+
+export const countTotal = async (req: Request, res: Response) => {
+  try {
+    const total = await Product.count({
+      where: { deleted: false },
+    });
+    return res.status(200).json(total);
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({ message: e.message });
+    }
+  }
+};
+
+export const getAll = async (req: Request, res: Response) => {
+  const { take, skip } = pagination(req);
+
+  try {
+    const products = await Product.findAndCount({
+      where: { deleted: false },
+      order: { id: "DESC" },
+      take,
+      skip,
+    });
+    const [items, count] = products;
+    return res.status(200).json({ items, count });
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({ message: e.message });
+    }
+  }
+};
+
+export const get = async (req: Request, res: Response) => {
+  try {
+    const id: number = Number.parseInt(req.params.id);
+    const product = await Product.findOneBy({ id });
+    if (!product)
+      return res.status(404).json({ message: "Product does not exist" });
+    return res.json(product);
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({ message: e.message });
+    }
+  }
+};
+
+export const update = async (req: Request, res: Response) => {
+  try {
+    const id: number = Number.parseInt(req.params.id);
+    const product = await Product.findOneBy({ id });
+    if (!product)
+      return res.status(404).json({ message: "Product does not exist" });
+    const { name, active } = req.body;
+    await Product.update({ id }, { name: name.toUpperCase(), active });
+    return res.sendStatus(204);
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({ message: e.message });
+    }
+  }
+};
+
+export const remove = async (req: Request, res: Response) => {
+  try {
+    const id: number = Number.parseInt(req.params.id);
+    const product = await Product.findOneBy({ id });
+    if (!product)
+      return res.status(404).json({ message: "Product does not exist" });
+    await Product.update({ id }, { deleted: true });
+    return res.sendStatus(204);
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({ message: e.message });
+    }
+  }
+};
