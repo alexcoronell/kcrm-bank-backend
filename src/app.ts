e99062e7644d37022aa699e3190787461523d4ec
Alex Coronell <alexcoronell@gmail.com>
2024-10-31 15:53:30 -0500

    Helmet installed and applied on app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 import "dotenv/config";
 
 /* Routes */
@@ -15,6 +16,7 @@ import userTypeRouter from "./routes/userType.router";
 const app = express();
 app.use(morgan("dev"));
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(franchiseRouter);
